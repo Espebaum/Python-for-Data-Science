598c8824fc1c31ae952792e38493163b47d6e319
Espebaum/gyopark <vocops5000@gmail.com>
2024-08-25 21:41:17 +0900

    py00 ex01

---
+++ py00/ex01/format_ft_time.py
@@ -0,0 +1,12 @@
+from time import time
+from time import localtime
+from time import ctime
+
+t = time()
+lct = localtime(t)
+format_t = f'{t:,.4f}' # f-string으로 숫자에 콤마 넣고 소숫점 4자리까지 확인
+str_time = ctime(t) # "Sun Aug 25 21:38:08 2024"
+split_time = str_time.split() # ['Sun', 'Aug', '25', '21:39:32', '2024']
+
+print("Seconds since January 1, 1970:", format_t, "or", "%.2e"%t, "in scientific notation")
+print(split_time[1], split_time[2], split_time[4])
