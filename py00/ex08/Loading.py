c945a3088f9ad18ab8a1f295b95fed159a3bd221
Espebaum <vocops5000@gmail.com>
2024-08-30 22:20:18 +0900

    py00

--- py00/ex08/Loading.py
+++ py00/ex08/Loading.py
@@ -1,2 +1,24 @@
 def ft_tqdm(lst: range) -> None:
-    
+    total = len(lst)
+    bar_length = 60
+
+    for i, item in enumerate(lst):
+        if i % 20 == 0 or i == total - 1:
+            progress = (i + 1) / total
+            filled_length = int(bar_length * progress)
+            bar = "█" * filled_length + ' ' * (bar_length - filled_length)
+            percent = int(progress * 100)
+
+            remaining_time = 1 - progress
+            it = 190
+            
+            # Format the elapsed and remaining time as MM:SS
+            progress_str = f'00:{int(progress):02}'
+            remaining_str =f'00:{int(remaining_time):02}' 
+
+            print(f'\r{percent}%|{bar}| {i + 1}/{total}', end='')
+            print(f' [{progress_str}<{remaining_str}, {it:.2f}it/s]', end='')
+
+        yield item
+
+    print()
--- py00/ex08/tester.py
+++ py00/ex08/tester.py
@@ -1,13 +1,15 @@
 from time import sleep
 from tqdm import tqdm
-# from Loading import ft_tqdm
+from Loading import ft_tqdm
 
 
-# for elem in ft_tqdm(range(333)):
-#     sleep(0.005)
-# print()
-
-for elem in tqdm(range(333)):
+for elem in tqdm(range(1000)):
     sleep(0.005)
 print()
 
+for elem in ft_tqdm(range(1000)):
+    sleep(0.005)
+print()
+
+
+# tqdm은 반복 가능한 객체를 감싸서 사용
