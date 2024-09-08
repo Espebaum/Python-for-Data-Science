a68a98df402191c94ca2aa059330a2286c1c2623
Espebaum <vocops5000@gmail.com>
2024-09-08 17:59:30 +0900

    py01 ex05

--- py01/ex05/load_image.py
+++ py01/ex05/load_image.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def ft_load(path: str) -> np.ndarray:
@@ -8,17 +7,6 @@ def ft_load(path: str) -> np.ndarray:
         img = Image.open(path)
         img_arr = np.array(img, dtype=np.int32)
         print("The shape of image is:", img_arr.shape)
-        print(img_arr)
-        img_cropped = img.crop((450, 100, 850, 500))
-        img_gray = img_cropped.convert('L')
-        img_arr = np.array(img_gray, dtype=np.int32)
-        img_arr = img_arr.reshape(400, 400)
-        img_transposed = list(zip(*img_arr))
-        img_transposed = np.array(img_transposed, dtype=np.int32)
-        print("New shape after slicing:", img_transposed.shape)
-        print(img_transposed)
-        plt.imshow(img_transposed, cmap='gray')
-        plt.show()
         return img_arr
 
     except Exception as e:
--- py01/ex05/pimp_image.py
+++ py01/ex05/pimp_image.py
@@ -1,2 +1,15 @@
-def ft_invert(str: path):
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def ft_invert(nd):
+    """
+    numpy 배열을 list로 바꾼 후 전치시킵니다.
+    """
+    lst = nd.tolist()
+    lst_A = list(reversed(lst))
+    n_A = np.array(lst_A, dtype=np.int32)
+    # print(n_A)
+    plt.imshow(n_A)
+    plt.show()
     
